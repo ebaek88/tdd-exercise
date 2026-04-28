7fa322e20cd69e8ee3ab64ab9ac014194c6a3420
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2026-04-28 17:45:45 -0600

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -77,8 +77,7 @@ function createApp(database) {
   }
 
   function isMonday(date) {
-    if(date instanceof Temporal.PlainDate) return date.dayOfWeek === 1;
-    return date.getUTCDay() === 1;
+    return date.dayOfWeek === 1;
   }
 
   function isHoliday(date) {
