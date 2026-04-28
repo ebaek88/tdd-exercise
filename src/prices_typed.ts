dc5bbba2f2a7847380202a7e77f8ea9f7fdd28b2
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2026-04-28 17:32:50 -0600

    tcr: tests pass

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -26,7 +26,8 @@ function createApp(database: Database) {
   });
 
   function parseDate(dateString: string | undefined): Date | Temporal.PlainDate | undefined {
-    if (dateString && typeof dateString === "string") {const temporalDate = Temporal.PlainDate.from(dateString);
+    if (dateString && typeof dateString === "string") {
+      const temporalDate = Temporal.PlainDate.from(dateString);
       return temporalDate;
     }
   }
